21a5ba3e18fd4abc3bed22add178128e3d3806d9
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-09-13 13:44:45 -0600

    feat(iac):create the ecr

---
+++ terraform/ecr.tf
@@ -0,0 +1,7 @@
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = "vite-docker-repository"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
