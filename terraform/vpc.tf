67f2538baedf9a663925d6ef77ccae13a85c73f1
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-09-13 15:33:09 -0600

    feat(iac): add group and listener to lb

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -60,6 +60,7 @@ resource "aws_alb" "vite_load_balancer" {
 }
 
 resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.strapi_vpc.id
   ingress {
     from_port   = 80
     to_port     = 80
@@ -80,4 +81,14 @@ resource "aws_lb_target_group" "target_group" {
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.strapi_vpc.id
+}
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_alb.vite_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
 }
