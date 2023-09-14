7e5f870a8275ad12e56d1325c1b5ea46ccdb1a24
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-09-14 15:46:22 -0600

    fix: add mutability to tag on ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = "vite-docker-repository"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
